f96406c5b853170133eb0f09f2df7419c77b8de1
Hongjun Lee <zunza96@gmail.com>
2024-03-27 16:27:12 +0900

    [FIX]: Github actions 추가

--- src/main/java/com/vandemarket/apigateway/VandemarketApigatewayApplication.java
+++ src/main/java/com/vandemarket/apigateway/VandemarketApigatewayApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import java.util.List;
+
 @SpringBootApplication
 @EnableEurekaClient
 public class VandemarketApigatewayApplication {
